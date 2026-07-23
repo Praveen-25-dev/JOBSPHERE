fdfe589504497e38e5738c640caddb420c1eabe6
Praveen-25-dev <praveen.sharma.dev25@gmail.com>
2026-07-23 11:35:44 +0530

    Updated project

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,5 +1,5 @@
 import NavBar from "../components/NavBar";
-import notifications from "../data/notifications";
+import notifications from "../data/Notifications";
 
 function Notifications() {
     const notifications =
